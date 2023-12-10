90f7aa0e37e8d2f1b97a4a3a3b3f0d6ebfd3bf82
pannaincze <pannaincze@gmail.com>
2023-12-10 11:02:32 +0100

    init project

---
+++ my-todo-app/src/test/java/com/codecool/mytodoapp/MyTodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.mytodoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
