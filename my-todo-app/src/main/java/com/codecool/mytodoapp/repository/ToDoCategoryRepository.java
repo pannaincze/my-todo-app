3c9f921f6fc0ad6f5fff8b2177d61b6a8836d8c6
pannaincze <pannaincze@gmail.com>
2024-01-12 13:15:47 +0100

    add method to find ToDoCategory by title to ToDoCategoryRepository

--- my-todo-app/src/main/java/com/codecool/mytodoapp/repository/ToDoCategoryRepository.java
+++ my-todo-app/src/main/java/com/codecool/mytodoapp/repository/ToDoCategoryRepository.java
@@ -3,5 +3,8 @@ package com.codecool.mytodoapp.repository;
 import com.codecool.mytodoapp.model.category.ToDoCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ToDoCategoryRepository extends JpaRepository<ToDoCategory, Long> {
+    Optional<ToDoCategory> findToDoCategoryByTitle(String title);
 }
