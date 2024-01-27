22afb923d9c6ac398a1498b2dbce2de115f92dc3
pannaincze <pannaincze@gmail.com>
2024-01-27 16:44:47 +0100

    add minor change to update the user's exp points by 10 when checking a todolist item

--- my-todo-app/src/main/java/com/codecool/mytodoapp/service/TodoListItemService.java
+++ my-todo-app/src/main/java/com/codecool/mytodoapp/service/TodoListItemService.java
@@ -2,6 +2,7 @@ package com.codecool.mytodoapp.service;
 
 import com.codecool.mytodoapp.model.notes.TodoList;
 import com.codecool.mytodoapp.model.notes.TodoListItem;
+import com.codecool.mytodoapp.model.user.User;
 import com.codecool.mytodoapp.repository.TodoListItemRepository;
 import com.codecool.mytodoapp.repository.TodoListRepository;
 import lombok.RequiredArgsConstructor;
@@ -12,9 +13,13 @@ import org.springframework.stereotype.Service;
 public class TodoListItemService {
     private final TodoListItemRepository todoListItemRepository;
     private final TodoListRepository todoListRepository;
+
     public TodoList editTodoListCheck(long todoListId, long todoListItemId) {
         TodoList todoList = todoListRepository.findById(todoListId).orElseThrow();
         TodoListItem todoListItem = todoListItemRepository.findById(todoListItemId).orElseThrow();
+
+        User user = todoList.getUser();
+        user.setExperiencePoints(user.getExperiencePoints() + 10);
 
         todoListItem.setChecked(!todoListItem.isChecked());
         todoListItemRepository.save(todoListItem);
